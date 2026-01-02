2dbca0f14f1527d1ed8d1228939f58f071d6f1e6
justinm0979 <justinm0979@gmail.com>
2026-01-02 14:57:45 -0600

    add message to pass to game that tells if guess is too high/low

--- number_guessing_game/main.cpp
+++ number_guessing_game/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "game.h"
+#include <functional>
+#include <string>
+#include <format>
 
 auto main(int argc, char** argv) -> int
 {
+  std::function<std::string(int, int)> guess_message = [](int number, int guess)
+  {
+    return std::format("Guess is too {}", (guess < number ? "low" : "high"));
+  };
+
   std::cout << "Working" << std::endl;
 }
